8ca8c63c51f67f61728783da6e587d7fd6b48741
ellnara <100730699+ellnara@users.noreply.github.com>
2022-04-07 19:25:32 +0400

    Add files via upload

---
+++ MyList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp10
+{
+    class MyList<T>
+    {
+        private int _count;
+        public int Count { get => _count; }
+
+        public int Capasity = 0;
+        public T[] array;
+        public MyList()
+        {
+            array = new T[0];
+        }
+        public MyList(int capasity)
+        {
+            if (capasity>0)
+            {
+                Capasity = capasity;
+            }
+        }
+        public void AddItem(T item)
+        {
+            Resizable(array.Length == _count);
+            array[Count] = item;
+            _count++;
+        }
+        public void AddItems(params T[] items)
+        {
+            foreach (var item in array)
+            {
+                AddItem(item);
+            }
+        }
+        public void Resizable(bool value)
+        {
+            if (value)
+            {
+                if (array.Length>0)
+                {
+                    Array.Resize(ref array, array.Length * 2);
+                }
+                else
+                {
+                    Array.Resize(ref array, array.Length + 2);
+                }
+            }
+        }
+        public void ShowList()
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            MyList<int> list = new MyList<int>(4);
+            list.AddItems(4,6,7);
+            list.ShowList();
+        }
+       
+    }
+}
